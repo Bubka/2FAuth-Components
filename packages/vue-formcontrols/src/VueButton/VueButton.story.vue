0222bbdf01e1915504985087acf589226a1bb629
Bubka <858858+Bubka@users.noreply.github.com>
2025-04-23 09:33:26 +0200

    Add VueButton & VueFormButtons stories

---
+++ packages/vue-formcontrols/src/VueButton/VueButton.story.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+    import { logEvent } from 'histoire/client'
+    import VueButton from './VueButton.vue'
+
+    function initState() {
+        return {
+            label: 'Label',
+        }
+    }
+</script>
+
+<template>
+    <Story :init-state="initState">
+        <template #default="{ state }">
+            <VueButton :label="state.label" @click="logEvent('Click',$event)" />
+        </template>
+    </Story>
+</template>
---
+++ packages/vue-formcontrols/src/VueFormButtons/VueFormButtons.story.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+    import { logEvent } from 'histoire/client'
+    import VueFormButtons from './VueFormButtons.vue'
+
+    function initState() {
+        return {
+            submitLabel: 'Submit',
+            cancelLabel: 'Cancel',
+            showCancelButton: true,
+        }
+    }
+</script>
+
+<template>
+    <Story :init-state="initState">
+        <template #default="{ state }">
+            <VueFormButtons
+                :submitLabel="state.submitLabel"
+                :cancelLabel="state.cancelLabel"
+                :showCancelButton="state.showCancelButton"
+                @click="logEvent('Click',$event)" />
+        </template>
+    </Story>
+</template>
