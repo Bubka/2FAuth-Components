0554ef33fe8bb608b768ee2e0a8442b516f7286c
Bubka <858858+Bubka@users.noreply.github.com>
2025-04-24 13:28:50 +0200

    Add a basic wrapper to stories

---
+++ histoire/Wrapper.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <!-- basic wrapper to get some padding around the story -->
+    <div style="padding: 0.25rem">
+        <slot />
+    </div>
+</template>
--- histoire/setup.ts
+++ histoire/setup.ts
@@ -1,1 +1,8 @@
 import '@2fauth/styles'
+
+import { defineSetupVue3 } from '@histoire/plugin-vue'
+import WrapperGlobal from './Wrapper.vue'
+
+export const setupVue3 = defineSetupVue3(({ addWrapper }) => {
+    addWrapper(WrapperGlobal)
+})
