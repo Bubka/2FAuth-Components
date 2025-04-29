92f9402d281260d12bd5a61c589435d7c5986b9f
Bubka <858858+Bubka@users.noreply.github.com>
2025-04-29 09:35:55 +0200

    Add FormSelect component & story

---
+++ packages/formcontrols/src/FormSelect/FormSelect.story.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+    import { logEvent } from 'histoire/client'
+    import FormSelect from './FormSelect.vue'
+
+    function initStateDefault() {
+        return {
+            model: 2,
+            options: [
+                { text: 'message.option_1', value: 1 },
+                { text: 'message.option_2', value: 2 },
+                { text: 'message.option_3', value: 3 },
+            ],
+            label: 'Password',
+            fieldname: 'MyFieldName',
+            errorMessage: 'This is the validation error text',
+            help: 'This is the help text',
+            isIndented: false,
+            isLocked: false,
+            showRules: true,
+        }
+    }
+</script>
+
+<template>
+    <Story :init-state="initStateDefault">
+        <template #default="{ state }">
+            <FormSelect
+                v-model="state.model"
+                :options="state.options"
+                @update:model-value="logEvent('update:model-value', $event)"
+                :label="state.label"
+                :fieldName="state.fieldname"
+                :errorMessage="state.errorMessage"
+                :help="state.help"
+                :is-indented="state.isIndented"
+                :is-locked="state.isLocked"
+            />
+        </template>
+    </Story>
+</template>
---
+++ packages/formcontrols/src/FormSelect/FormSelect.vue
@@ -0,0 +1,72 @@
+<script setup>
+    import { ref } from 'vue'
+    import { FormFieldError } from '../FormFieldError'
+    import { useIdGenerator, useValidationErrorIdGenerator } from '../helpers'
+    import { LucideChevronRight, LucideLock } from 'lucide-vue-next'
+
+    const props = defineProps({
+        modelValue: [String, Number, Boolean],
+        label: {
+            type: String,
+            default: ''
+        },
+        fieldName: {
+            type: String,
+            default: '',
+            required: true
+        },
+        errorMessage: [String],
+        options: {
+            type: Array,
+            required: true
+        },
+        help: {
+            type: String,
+            default: ''
+        },
+        isIndented: Boolean,
+        isDisabled: Boolean,
+        isLocked: Boolean,
+        idSuffix: {
+            type: String,
+            default: ''
+        },
+    })
+
+    const selected = ref(props.modelValue)
+    const { inputId } = useIdGenerator('select', props.fieldName + props.idSuffix)
+    const { valErrorId } = useValidationErrorIdGenerator(props.fieldName)
+    const legendId = useIdGenerator('legend', props.fieldName + props.idSuffix).inputId
+</script>
+
+<template>
+    <div class="field is-flex">
+        <div v-if="isIndented" class="pr-1" :class="{ 'is-opacity-5' : isDisabled || isLocked }">
+            <LucideChevronRight class="has-text-grey rotate-135" />
+        </div>
+        <div>
+            <label :for="inputId" class="label" :class="{ 'is-opacity-5' : isDisabled || isLocked }">
+                {{ label }}<LucideLock v-if="isLocked" class="ml-2 icon-size-1" />
+            </label>
+            <div class="control">
+                <div class="select">
+                    <select
+                        :id="inputId"
+                        v-model="selected"
+                        v-on:change="$emit('update:modelValue', $event.target.value)"
+                        :disabled="isDisabled || isLocked"
+                        :aria-describedby="help ? legendId : undefined"
+                        :aria-invalid="errorMessage != undefined"
+                        :aria-errormessage="errorMessage != undefined ? valErrorId : undefined" 
+                    >
+                        <option v-for="option in options" v-bind:key="option.value" :value="option.value">
+                            {{ $t(option.text) }}
+                        </option>
+                    </select>
+                </div>
+            </div>
+            <FormFieldError v-if="errorMessage != undefined" :error="errorMessage" :field="fieldName" />
+            <p :id="legendId" class="help" v-html="$t(help)" v-if="help"></p>
+        </div>
+    </div>
+</template>
