c7b9e1e59f16f418bba710a6b3d267ebfe24a27e
Bubka <858858+Bubka@users.noreply.github.com>
2025-05-22 08:33:06 +0200

    Add SearchBox component & story

---
+++ packages/ui/src/SearchBox/SearchBox.story.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+    import { logEvent } from 'histoire/client'
+    import SearchBox from '../SearchBox/SearchBox.vue'
+
+    function initState() {
+        return {
+            keyword: 'keyword',
+            isSmall: false,
+            hasNoBackground: false,
+            placeholder: 'placeholder',
+        }
+    }
+</script>
+
+<template>
+    <Story :init-state="initState">
+        <template #default="{ state }">
+            <SearchBox
+                v-model:keyword="state.keyword"
+                ref="searchBox"
+                :is-small="state.isSmall"
+                :hasNoBackground="state.hasNoBackground"
+                :placeholder="state.placeholder"
+                @cleared="logEvent('cleared', $event)"
+            />
+        </template>
+    </Story>
+</template>
---
+++ packages/ui/src/SearchBox/index.js
@@ -0,0 +1,3 @@
+export {
+    default as SearchBox
+} from './SearchBox.vue'
