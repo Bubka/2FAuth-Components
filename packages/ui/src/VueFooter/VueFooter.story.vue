3e730868d6d2150588540af710210d58c96a9ab3
Bubka <858858+Bubka@users.noreply.github.com>
2025-05-22 14:19:50 +0200

    Add VueFooter component & story

---
+++ packages/ui/src/VueFooter/VueFooter.story.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+    import VueFooter from './VueFooter.vue'
+
+    function initStateForButtons() {
+        return {
+            showButton: true,
+        }
+    }
+
+    function initStateGeneric() {
+        return {
+            showButton: false,
+        }
+    }
+</script>
+
+<template>
+    <Story>
+        <Variant title="Buttons" :init-state="initStateForButtons">
+            <template #default="{ state }">
+                <VueFooter :show-buttons="state.showButton">
+                    <button class="button">Some</button>
+                    <button class="button">grouped</button>
+                    <button class="button">buttons</button>
+                </VueFooter>
+            </template>
+        </Variant>
+        <Variant title="Generic" :init-state="initStateGeneric">
+            <template #default="{ state }">
+                <VueFooter :show-buttons="state.showButton">
+                    <p>This is a random html tag</p>
+                </VueFooter>
+            </template>
+        </Variant>
+    </Story>
+</template>
---
+++ packages/ui/src/VueFooter/VueFooter.vue
@@ -0,0 +1,26 @@
+<script setup>
+    const props = defineProps({
+        showButtons: {
+            type: Boolean,
+            default: false
+        },
+    })
+
+</script>
+
+<template>
+    <footer class="main">
+        <!-- action buttons -->
+        <div class="columns is-gapless" v-if="showButtons == true">
+            <div class="column has-text-centered">
+                <div class="field is-grouped">
+                    <slot></slot>
+                </div>
+            </div>
+        </div>
+        <!-- sub-links -->
+        <div v-else class="content has-text-centered">
+            <slot></slot>
+        </div>
+    </footer>
+</template>
