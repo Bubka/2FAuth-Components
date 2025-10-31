7ca0a4efecee5655e879bf5a156f449f5d8abe0c
Bubka <858858+Bubka@users.noreply.github.com>
2025-10-31 15:46:19 +0100

    Add group-less option to GroupSwitch component

--- packages/ui/src/GroupSwitch/GroupSwitch.vue
+++ packages/ui/src/GroupSwitch/GroupSwitch.vue
@@ -16,10 +16,16 @@
      * Sets the selected group
      */
     function setActiveGroup(id) {
-        activeGroup.value = id
         isVisible.value = false
 
-        emit('active-group-changed', id)
+        if (id > -1) {
+            activeGroup.value = id
+            emit('active-group-changed', id)
+        }
+        else {
+            emit('show-group-less', id)
+        }
+
     }
 
 </script>
@@ -30,11 +36,26 @@
             <div class="column is-one-third-tablet is-one-quarter-desktop is-one-quarter-widescreen is-one-quarter-fullhd">
                 <div class="columns is-multiline">
                     <UseColorMode v-slot="{ mode }">
-                        <div class="column is-full" v-for="group in groups" :key="group.id">
-                            <button type="button" class="button is-fullwidth" :class="{'is-dark has-text-light is-outlined': mode == 'dark'}" @click="setActiveGroup(group.id)">
-                                {{ group.id == 0 ? $t('label.all') : group.name }}
+                        <div class="column is-full">
+                            <button type="button" class="button is-fullwidth" :class="{'is-dark has-text-light is-outlined': mode == 'dark'}" @click="setActiveGroup(0)">
+                                {{ $t('label.all_accounts') }}
                             </button>
                         </div>
+                        <div class="column is-full">
+                            <button type="button" class="button is-fullwidth" :class="{'is-dark has-text-light is-outlined': mode == 'dark'}" @click="setActiveGroup(-1)">
+                                {{ $t('label.group_less_accounts') }}
+                            </button>
+                        </div>
+                        <div class="column has-text-centered">
+                            {{ $t('message.or_filter_by_group') }}
+                        </div>
+                        <template v-for="group in groups" :key="group.id">
+                            <div v-if="group.id > 0" class="column is-full">
+                                <button type="button" class="button is-fullwidth" :class="{'is-dark has-text-light is-outlined': mode == 'dark'}" @click="setActiveGroup(group.id)">
+                                    {{ group.name }}
+                                </button>
+                            </div>
+                        </template>
                     </UseColorMode>
                 </div>
                 <div v-if="$slots.default" class="columns is-centered">
